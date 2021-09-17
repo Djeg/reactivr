f48195dacbef114855999a3d785bf24d5418510c
David Jegat <david.jegat@gmail.com>
2021-09-17 10:01:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ReactivR
+
+ReactivR is a react library for managing states and effects
+easily in a reactive way.
---
+++ __tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { TEST } from '../src/index'
+
+it('Define a test constant', () => {
+  expect(TEST).toBe('test')
+})
