37ef2a1a86eb3ef0215bea6b123c34355fb15a7c
David Jegat <david.jegat@gmail.com>
2021-10-06 13:45:22 +0200

    Finish the index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { action, produce, reduce, when } from './actions'
+import { action, DEFAULT_ID, produce, reduce, when } from './actions'
 import { simpleEffectRunner } from './effect'
 import { compose } from './functions/compose'
 import { pipe } from './functions/pipe'
@@ -10,17 +10,51 @@ import {
 import { useActionEvent, useModule, useSelector, useStore } from './react/hooks'
 import { Provider, StoreContext } from './react/Provider'
 import { Render } from './react/Render'
-import { selector } from './selector'
-import { createStore } from './store'
-import { SimpleEffect, ViewComponent } from './types'
+import { select, selector } from './selector'
+import { createStore, StoreOptions } from './store'
+import {
+  Action,
+  ActionContainer,
+  ActionContainerEnhancer,
+  ActionCreator,
+  ActionReducer,
+  Dispatch,
+  EmptyAction,
+  LightStore,
+  ReactiveModule,
+  Selector,
+  SelectorContainer,
+  SelectorSubject,
+  SimpleEffect,
+  StateCollector,
+  StoreExtension,
+  ViewComponent,
+} from './types'
 
 export {
   action,
   SimpleEffect,
   simpleEffectRunner,
+  Action,
+  ActionCreator,
+  Dispatch,
+  ActionReducer,
+  LightStore,
+  StoreExtension,
+  ActionContainer,
+  ActionContainerEnhancer,
+  StateCollector,
+  SelectorSubject,
+  Selector,
+  SelectorContainer,
+  ReactiveModule,
+  EmptyAction,
+  StoreOptions,
+  DEFAULT_ID,
   when,
   reduce,
   produce,
+  select,
   ViewComponent,
   selector,
   Provider,
